73b84fc4914c0b0f2efc8ebcf5a1dcb620d64e91
Licht Hong <duncanhong@outlook.com>
2025-04-14 12:36:04 +0800

    修改 favicon

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Hikaru",
-	subtitle: "Hello world",
+	subtitle: "hello world",
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -54,7 +54,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Hikaru",
-	bio: "Hi i am hikaru.",
+	bio: "Hello World",
 	links: [
 		{
 			name: "GitHub",
