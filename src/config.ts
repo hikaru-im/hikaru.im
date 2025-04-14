f9bec0eaa91f396fdb22a22e3779db5ed2cfbf2b
Licht Hong <duncanhong@outlook.com>
2025-04-14 16:38:27 +0800

    压缩头像以提升加载速度并改为webp格式

--- src/config.ts
+++ src/config.ts
@@ -43,7 +43,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Hikaru",
 	bio: "Hello World",
 	links: [
